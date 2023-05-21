47162162766dfc3379d3cd8c48499ccee2efd7f2
OneOldMill <133509455+OneOldMill@users.noreply.github.com>
2023-05-21 19:31:46 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -7,16 +7,18 @@ function myImageView(ver) {
      var myImageList = document.getElementById("myImageList");
      var myImageValue = myImageList.options[myImageList.selectedIndex].value;
      var myImageText = myImageList.options[myImageList.selectedIndex].text;
-     //myImageFile = "images/" + myImageText + ".jpg";
-     myImageFile = "/images/" + myImageText + ".jpg";
+     myImageFile = "images/" + myImageText + ".jpg";
+     //myImageFile = "/images/" + myImageText + ".jpg";
      //alert("myImageFile java att= "+myImageFile);
      var img = document.createElement("img");
+//     var img = new image();
      img.src = myImageFile;
 //     img.width = 250;
 //     img.height = 300;
 //     document.write("\n");
 //     document.writeln();
-//     document.body.appendChild(img);
+     document.body.appendChild(img);
+//     document.appendChild(img);
      open(myImageFile, '_blank', "School");
 }
 
